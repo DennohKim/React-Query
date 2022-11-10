46933655ee28d631741062ec384b15ad12b9d482
“dennohkim” <kimathidennis38@gmail.com>
2022-11-10 18:09:19 +0300

    making first query

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useQuery } from 'react-query';
+
+function fetchUser(username){
+  return fetch(`https://api.github.com/users/${username}`)
+  .then(res => res.json())
+
+}
+
+function GithubUser({username}){
+  //useQuery accepts 2 arguments. Query key which is an array that keeps track of query in the cache and query function that fetches the data
+  const userQuery = useQuery(
+    [username], 
+    () => fetchUser(username)
+     
+  )
+
+  //React query will fetch the data and make it available in userQuery.data property
+  const data = userQuery.data;
+
+  if(userQuery.isLoading) return <p>Loading...</p>
+
+  if (userQuery.isError) return <p>Error: {userQuery.error.message}</p>
+
+  return  (
+    <pre>{JSON.stringify(data, null, 2)}</pre>
+  )
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+ 
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <GithubUser username="uidotdev"/>
+           
     </div>
   )
 }
